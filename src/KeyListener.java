257cbf11f9d92eb71e559af92fd20e8fcd1818c1
iiiieo <achtbyte01@gmail.com>
2020-10-28 10:22:51 +0100

    keylistener test

---
+++ src/KeyListener.java
@@ -0,0 +1,20 @@
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+
+public class KeyListener implements NativeKeyListener {
+    @Override
+    public void nativeKeyTyped(NativeKeyEvent e) {
+
+    }
+
+    @Override
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        System.out.println("Key::"+e.getKeyText(e.getKeyCode()));
+    }
+
+    @Override
+    public void nativeKeyReleased(NativeKeyEvent e) {
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        new KeyListener();
         Window w = Window.getVirusWindow(1000, 1000);
         w.getCanvas().paintComponent(w.getGraphics());
         w.repaint();
